78622cc8a573349ef8b7b276141d5ca15fe52ebb
caeser lettuce <84699501+caeserlettuce@users.noreply.github.com>
2024-02-07 19:06:29 -0800

    ja

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ var static_bounds = [];
 var active_bounds = [];
 var setting_choices = {};
 var show_debug = false;
+var show_ghost = true;
+var board_width = 10;
+var board_height = 20;
+var game_score = 0;
 var game_paused = false;
 var game_status = false;
 var game_interval;
+var slam_interval;
 var timeouts = {
   "keypresses": {}
 };
@@ -48,6 +53,10 @@ function rand(array) {
 
 function is_element(element) {
   return element instanceof Element || element instanceof HTMLDocument;  
+}
+
+function copy_json(json_tm) {
+  return JSON.parse(JSON.stringify(json_tm))
 }
 
 function clear_active_board() {
@@ -94,10 +103,74 @@ function display_ghost_debug(points, light) {
   }
 }
 
+function display_ghost_block(data) {
+  if (show_ghost == true) {
+    var data_new = copy_json(data);
+    var ghost_height = find_slam_height(data);
+    data_new["loc"][1] = ghost_height
+    var ghost_points = get_piece_points(data_new);
+    
+    for (i in ghost_points) {
+      var node = document.querySelector(".template .ghost").cloneNode(true);
+      node.style = `transform: translate(${ghost_points[i][0]}px, ${ghost_points[i][1]}px);`;
+      active_board.appendChild(node);
+    }
+
+  }
+}
+
+function check_lines() {
+
+  var lines = {};
+  var lines_clear = [];
+  
+  for (i in gamesave["static"]) {
+    var x = gamesave["static"][i]["loc"][0];
+    var y = gamesave["static"][i]["loc"][1];
+
+    if (!lines[y]) {
+      lines[y] = [];
+    }
+    lines[y].push(x);
+  }
+  for (i in lines) {
+    if (lines[i].length == 10) {
+      lines_clear.push(parseInt(i));
+    }
+  }
+  console.log(lines);
+  console.log(lines_clear);
+
+  var new_static = [];
+  var lines_cleared = lines_clear.length;
+
+  for (i in gamesave["static"]) {
+    var y = gamesave["static"][i]["loc"][1];
+    if (lines_clear.includes(y)) {
+      console.log(y);
+      game_score += 1;
+
+      // do NOT add it because it is being removed
+
+    } else {
+      var new_piece_entry = copy_json(gamesave["static"][i]);
+      if (y < lines_clear[0]) {
+        new_piece_entry["loc"][1] += lines_cleared;
+      }
+      new_static.push(new_piece_entry);
+    }
+  }
+  gamesave["static"] = copy_json(new_static);
+}
+
 function display_game(game) {
   // for (i in game["pieces"]) {
   //   display_shape(game["pieces"][i]);
   // }
+  if (game_paused == false) {
+    check_lines();
+  }
+
   for (i in game["static"]) {
     var x = game["static"][i]["loc"][0];
     var y = game["static"][i]["loc"][1];
@@ -107,6 +180,7 @@ function display_game(game) {
     active_board.appendChild(node);
   }
 
+  display_ghost_block(game["active piece"]);
   display_shape(game["active piece"]);
 }
 
@@ -128,7 +202,7 @@ function piece_rotate(piece_in) {
 }
 
 function get_piece_points(data) {
-  console.log(data);
+  //console.log(data);
   var type = data["type"]
   var loc = data["loc"];
   var rot = data["rot"];
@@ -145,6 +219,23 @@ function get_piece_points(data) {
     points_out.push(the_set);
   }
   //console.log(points_out);
+  return points_out
+}
+
+function get_piece_max_bounds(data) {
+  var type = data["type"]
+  var loc = data["loc"];
+  var rot = data["rot"];
+  var raw_points = [...pieces[type][rot]];
+  var points_out = [0, 0];
+  for (i in raw_points) {
+    if (raw_points[i][0] > points_out[0]) {
+      points_out[0] = raw_points[i][0];
+    }
+    if (raw_points[i][1] > points_out[1]) {
+      points_out[1] = raw_points[i][1];
+    }
+  }
   return points_out
 }
 
@@ -193,13 +284,14 @@ function check_bounds(piece) {
   return final
 }
 
-function new_piece() {
-  add_static(gamesave["active piece"]);
-  gamesave["active piece"] = {
-    "type": `${rand(piece_types)}`,
-    "loc": [0,0],
-    "rot": 0
+function new_piece(first) {
+  if (first != true) {
+    add_static(gamesave["active piece"]);
   }
+  var the_piece = rand(piece_types);
+  gamesave["active piece"] = {"type": `${the_piece}`, "rot": 0, "loc": [0,0]}
+  var piece_bounds = get_piece_max_bounds(gamesave["active piece"]);
+  gamesave["active piece"]["loc"][0] = Math.floor(board_width / 2) - (Math.floor(piece_bounds[0] / 2) + 1 ); // put it at the center
 }
 
 
@@ -251,7 +343,8 @@ function game_speed(speed) {
 
 function start_game() {
 
-  add_static({"type": "lblue", "loc": [0,19], "rot": 0 })
+  //add_static({"type": "lblue", "loc": [0,19], "rot": 0 })
+  new_piece(true);
   game_status = true;
   game_speed(500);
   
@@ -336,9 +429,89 @@ function user_rotate() {
     gamesave["active piece"] = {...rotate_new};
     clear_active_board();
     display_game(gamesave);
+  } else if (bounds_rotate_check == false) {  // if the shape is right up against the edge, figure out how to rotate it so its good
+    var max_bounds = get_piece_max_bounds(rotate_new);
+    var points = get_piece_points(gamesave["active piece"]);
+    var in_left = false;
+    var in_right = false;
+    
+    for (i in points) {                   // check if at the wall or not
+      if (points[i][0] < max_bounds[0]) {
+        // up against left wall
+        in_left = true;
+        console.log("left wall!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      } else if (points[i][0] > 9 - max_bounds[0]) {
+        // up against right wall
+        in_right = true
+        console.log("right wall!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      }
+    }
+
+    if (in_left == true) {
+      rotate_new["loc"][0] = 0;
+      var bounds_rotate_check_2 = check_bounds(rotate_new);
+      if (bounds_rotate_check_2 == true) {
+        gamesave["active piece"] = copy_json(rotate_new);
+        clear_active_board();
+        display_game(gamesave);
+      }
+    } else if (in_right == true) {
+      console.log("before", rotate_new["loc"][0]);
+      rotate_new["loc"][0] = 9 - max_bounds[0];  // as much tm
+      console.log("after", rotate_new["loc"][0]);
+      var bounds_rotate_check_2 = check_bounds(rotate_new);
+      if (bounds_rotate_check_2 == true) {
+        gamesave["active piece"] = copy_json(rotate_new);
+        clear_active_board();
+        display_game(gamesave);
+      }
+    }
   }
 }
 
+function find_slam_height(data) {
+  var height_out = 20;
+  var height_try = 20;
+  var height_found = false;
+  var highest_point = data["loc"][1];
+  var slam_new_try = copy_json(data);
+  for (let i = 0; i < 20; i++) {
+    height_try += -1;
+    if (height_found == false) {
+      height_out = height_try
+    }
+    slam_new_try["loc"][1] = height_try;
+    //console.log(slam_new["loc"])
+    //console.log(height_out);
+    var slam_bounds_check = check_bounds(slam_new_try);
+    if (slam_bounds_check == true) {
+      height_found = true;
+    } else {
+      if (height_try >= highest_point) {
+        height_found = false;
+      }
+    }
+  }
+  return height_out
+}
+
+/*
+
+  */
+
+function user_slam() {
+  
+  var slam_height = find_slam_height(gamesave["active piece"]);
+  console.log(slam_height)
+  var slam_new = copy_json(gamesave["active piece"]);
+  slam_new["loc"][1] = slam_height;
+  gamesave["active piece"] = copy_json(slam_new);
+  new_piece();
+  clear_active_board();
+  display_game(gamesave);
+  
+
+}
 
 document.addEventListener('keydown', (event) => {
   var name = event.key;
@@ -365,6 +538,14 @@ document.addEventListener('keydown', (event) => {
           document.querySelector(".pausemenu").style.display = "none";
         }, 260);
         game_paused = false;
+      }
+    } else if (keyid == 38) { // up
+      if (game_paused == false) {
+        user_rotate();
+      }
+    } else if (keyid == 32) { // space
+      if (game_paused == false) {
+        user_slam();
       }
     } else if (keyid == 67) { // c
   
@@ -407,20 +588,15 @@ window.addEventListener("keypressed", function (event) {
   case 37:        // left
     var key = 37;
     //console.log(key_delays[`${key}`]);
-    do_thingy(37, () => {user_move([-1,0])} );
-    break;
-  case 38:        // up (rotate)
-    do_thingy(38, () => {user_rotate()} );
+    do_thingy(37, () => {if (game_paused == false) { user_move([-1,0]) }} );
     break;
   case 39:        // right
-    do_thingy(39, () => {user_move([1,0])} );
+    do_thingy(39, () => {if (game_paused == false) { user_move([1,0]) }} );
     break;
   case 40:        // down
-    do_thingy(40, () => {user_move([0,1])} );
-    break;
-  case 32:        // space
-
+    do_thingy(40, () => {if (game_paused == false) { user_move([0,1]) }} );
     break;
   }
   //console.log(event.keyCode);
 }, false);
+
